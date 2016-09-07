5f489f2834c4d1977dc7129953d277e7316ad399
lucas <luca.pisani@abstract.it>
2016-09-08 00:39:02 +0200

    add VAGRANTUID to override the standard configuration

--- common/prepare-vagrant-user.sh
+++ common/prepare-vagrant-user.sh
@@ -4,6 +4,7 @@ set -e
 source common/ui.sh
 
 export VAGRANT_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+VAGRANTUID=${VAGRANTUID:-1000}
 
 info "Preparing vagrant user..."
 
@@ -12,14 +13,19 @@ if $(grep -q 'vagrant' ${ROOTFS}/etc/shadow); then
   log 'Skipping vagrant user creation'
 elif $(grep -q 'ubuntu' ${ROOTFS}/etc/shadow); then
   debug 'vagrant user does not exist, renaming ubuntu user...'
+  OLDUID=`chroot ${ROOTFS} id -u ubuntu`  # looks up current (old) uid
   mv ${ROOTFS}/home/{ubuntu,vagrant}
   chroot ${ROOTFS} usermod -l vagrant -d /home/vagrant ubuntu &>> ${LOG}
   chroot ${ROOTFS} groupmod -n vagrant ubuntu &>> ${LOG}
+  chroot ${ROOTFS} usermod -u $VAGRANTUID vagrant &>> ${LOG}
+  chroot ${ROOTFS} groupmod -g $VAGRANTUID vagrant &>> ${LOG}
+  chroot ${ROOTFS} find / -uid $OLDUID -exec chown -h $VAGRANTUID {} + &>> ${LOG}
+  chroot ${ROOTFS} find / -gid $OLDUID -exec chgrp -h $VAGRANTUID {} + &>> ${LOG}
   echo -n 'vagrant:vagrant' | chroot ${ROOTFS} chpasswd
   log 'Renamed ubuntu user to vagrant and changed password.'
 elif [ ${DISTRIBUTION} = 'centos' -o ${DISTRIBUTION} = 'fedora' ]; then
   debug 'Creating vagrant user...'
-  chroot ${ROOTFS} useradd --create-home -s /bin/bash -u 1000 vagrant &>> ${LOG}
+  chroot ${ROOTFS} useradd --create-home -s /bin/bash -u $VAGRANTUID vagrant &>> ${LOG}
   echo -n 'vagrant:vagrant' | chroot ${ROOTFS} chpasswd
   sed -i 's/^Defaults\s\+requiretty/# Defaults requiretty/' $ROOTFS/etc/sudoers
   if [ ${RELEASE} -eq 6 ]; then
@@ -30,7 +36,7 @@ elif [ ${DISTRIBUTION} = 'centos' -o ${DISTRIBUTION} = 'fedora' ]; then
   fi
 else
   debug 'Creating vagrant user...'
-  chroot ${ROOTFS} useradd --create-home -s /bin/bash vagrant &>> ${LOG}
+  chroot ${ROOTFS} useradd --create-home -u $VAGRANTUID -s /bin/bash vagrant &>> ${LOG}
   chroot ${ROOTFS} adduser vagrant sudo &>> ${LOG}
   echo -n 'vagrant:vagrant' | chroot ${ROOTFS} chpasswd
 fi
